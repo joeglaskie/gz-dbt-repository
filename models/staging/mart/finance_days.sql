020151de8be0ff753dd5f844684ea4fc92e5ff06
Joe Glaskie <joeglaskie08@gmail.com>
2024-09-11 15:12:03 +0000

    materialise into a table

--- models/staging/mart/finance_days.sql
+++ models/staging/mart/finance_days.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table')}}
+
 SELECT date_date,
 COUNT(orders_id) AS nb_transactions,
 ROUND(SUM(revenue),2) AS total_revenue,
@@ -7,5 +9,5 @@ ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
 ROUND(SUM(shipping_fee),2) AS total_shipping_fee,
 ROUND(SUM(logcost),2) AS total_log_cost,
 SUM(quantity) AS total_quantity_sold
-FROM `local-bastion-433009-h6.dbt_joe.int_orders_operational`  
+FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
