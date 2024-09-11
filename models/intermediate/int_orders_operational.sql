1cccfadd23e3daf8655c81e0b532c7296fd71ac8
Joe Glaskie <joeglaskie08@gmail.com>
2024-09-11 11:46:17 +0000

    including ship details

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,4 +1,4 @@
-SELECT m.*,
+SELECT *,
 ROUND((m.margin + s.shipping_fee - s.logcost - s.ship_cost),2) AS operational_margin,
 FROM {{ ref('int_orders_margin') }} AS m
 LEFT JOIN {{ ref('stg_raw__ship') }} AS s
